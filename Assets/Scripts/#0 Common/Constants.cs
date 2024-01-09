f128891d90d6eaf2d7af1ba0ea7cee4da6cf2b39
Gyokujin <dd43454548@naver.com>
2024-01-10 05:21:14 +0900

    # 랭킹 기능 업데이트

--- Assets/Scripts/#0 Common/Constants.cs
+++ Assets/Scripts/#0 Common/Constants.cs
@@ -1,7 +1,8 @@
 public class Constants
 {
     public static readonly string USER_DATA_TABLE = "USER_DATA";
-    public static readonly string DAILY_RANK_UUID = "bd8ebcc0-1261-11ee-a578-cbb9bad56300";
+    public static readonly string USER_RANK_DATA_TABLE = "USER_RANK_DATA";
+    public static readonly string DAILY_RANK_UUID = "98bea6f0-af2b-11ee-862d-f913df39456d";
     public static readonly int MAX_RANK_LIST = 20;
 
     public static readonly string LEVEL_CHART = "85845";
--- Assets/Scripts/#100 Backend/DailyRankRegister.cs
+++ Assets/Scripts/#100 Backend/DailyRankRegister.cs
@@ -5,7 +5,6 @@ public class DailyRankRegister : MonoBehaviour
 {
     public void Process(int newScore)
     {
-        // UpdateMyRankData(newScore);
         UpdateMyBestRankData(newScore);
     }
 
@@ -14,7 +13,7 @@ public class DailyRankRegister : MonoBehaviour
         string rowInDate = string.Empty;
 
         // 랭킹 데이터를 업데이트하려면 게임 데이터에서 사용하는 데이터의 inDate 값이 필요
-        Backend.GameData.GetMyData(Constants.USER_DATA_TABLE, new Where(), callback =>
+        Backend.GameData.GetMyData(Constants.USER_RANK_DATA_TABLE, new Where(), callback =>
         {
             if (!callback.IsSuccess())
             {
@@ -39,7 +38,7 @@ public class DailyRankRegister : MonoBehaviour
                 { "dailyBestScore", newScore}
             };
 
-            Backend.URank.User.UpdateUserScore(Constants.DAILY_RANK_UUID, Constants.USER_DATA_TABLE, rowInDate, param, callback =>
+            Backend.URank.User.UpdateUserScore(Constants.DAILY_RANK_UUID, Constants.USER_RANK_DATA_TABLE, rowInDate, param, callback =>
             {
                 if (callback.IsSuccess())
                 {
@@ -47,7 +46,7 @@ public class DailyRankRegister : MonoBehaviour
                 }
                 else
                 {
-                    Debug.LogError($"랭킹 등록에 실패했습니다. : {callback}");
+                    Debug.LogError($"랭킹 등록 중 오류가 발생했습니다 : {callback}");
                 }
             });
         });
