77f289704ade6d8c0088c315e596beea11815e31
Gyokujin <dd43454548@naver.com>
2024-01-11 02:42:27 +0900

    # 뒤끝 보상 데이터 수정

--- Assets/Scripts/#0 Common/Constants.cs
+++ Assets/Scripts/#0 Common/Constants.cs
@@ -2,7 +2,7 @@ public class Constants
 {
     public static readonly string USER_DATA_TABLE = "USER_DATA";
     public static readonly string USER_RANK_DATA_TABLE = "USER_RANK_DATA";
-    public static readonly string DAILY_RANK_UUID = "98bea6f0-af2b-11ee-862d-f913df39456d";
+    public static readonly string DAILY_RANK_UUID = "2a363400-af94-11ee-862d-f913df39456d";
     public static readonly int MAX_RANK_LIST = 20;
 
     public static readonly string LEVEL_CHART = "85845";
